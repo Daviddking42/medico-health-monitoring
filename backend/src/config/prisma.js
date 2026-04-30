b392cb6d927c20d053971536e46490b92dc6852c
Daviddking42 <154324094+Daviddking42@users.noreply.github.com>
2026-04-30 05:43:49 +0100

    Configure production fallbacks for Vercel: backend/src/config/prisma.js

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,5 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
-
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL || "mysql://root:qBEhvMlNfOWUCSduxANCzxdFouhWUVtL@switchyard.proxy.rlwy.net:40591/railway",
+    },
+  },
+});
 module.exports = prisma;
